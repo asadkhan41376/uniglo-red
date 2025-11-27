7f41f002212ed9848574b40b3547b6b48922e2f3
mohammad asad <asadkhan41376@gmail.com>
2025-11-27 11:41:46 +0530

    initial commit

---
+++ assets/js/lenis.js
@@ -0,0 +1,14 @@
+
+  const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1 - Math.pow(2, -10 * t)),
+    smooth: true,
+  });
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+
